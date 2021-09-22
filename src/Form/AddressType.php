acb9f261050816e2cb2d223358f30b747afc4d0a
liohumb <helloliohumb@gmail.com>
2021-09-22 14:36:55 +0200

    création d'un formulaire address & edition et suppession des addresses depuis la vue address

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Address;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +16,65 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('company')
-            ->add('address')
-            ->add('postal')
-            ->add('city')
-            ->add('country')
-            ->add('phone')
-            ->add('user')
+            ->add('name', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'placeholder' => 'Nommez votre adresse'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'placeholder' => 'Prenom du destinataire'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'placeholder' => 'Nom du destinataire'
+                ]
+            ])
+            ->add('company', TextType::class, [
+                'label' => ' ',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Nom de la société (falcutatif)'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'placeholder' => 'Adresse'
+                ]
+            ])
+            ->add('postal', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'placeholder' => 'Code postal'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'placeholder' => 'Ville'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => ' ',
+                'preferred_choices' => ['FR'],
+                'attr' => [
+                    'placeholder' => 'Pays'
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'placeholder' => 'Téléphone'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider'
+            ])
         ;
     }
 
